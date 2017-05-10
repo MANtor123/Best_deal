16bf42706a55017eaabcf8e38404fe864af32f12
Mponeng <mantombiratego@gmail.com>
2017-05-10 14:56:25 +0200

    splited string

--- test/avo.test.js
+++ test/avo.test.js
@@ -3,5 +3,10 @@ var assert = require ('assert')
 
 describe('The avo function', function(){
 
+  it('should split the string', function(){
+    var string = ("1 for R3, 2 for R7, 3 for R10, 5 for R14.50.")
+    var stringSplit = string.split(',')
+  });
+
   
 });
